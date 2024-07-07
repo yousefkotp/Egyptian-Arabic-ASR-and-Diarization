b21c0fc9248c0adf43dd168a9c4b9f456d84f8e3
Karim Alaa <karimalaa1912@gmail.com>
2024-07-07 20:19:38 +0300

    extra spacing in install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pip install text-unidecode
 
 
 ## Install NeMo
-BRANCH = 'r2.0.0rc0'
+BRANCH='r2.0.0rc0'
 python -m pip install git+https://github.com/NVIDIA/NeMo.git@$BRANCH#egg=nemo_toolkit[asr]
 
 pip install -r requirements.txt
